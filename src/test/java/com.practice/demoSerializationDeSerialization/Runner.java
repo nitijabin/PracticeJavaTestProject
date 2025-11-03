57d961f0a202f06e0843398ad87049b9fb45764b
nitijabin <niti.jabin@rocketbnk.com>
2025-11-03 10:58:50 -0500

    deserialization and serialization practice class added

--- src/test/java/com.practice/demoSerializationDeSerialization/Runner.java
+++ src/test/java/com.practice/demoSerializationDeSerialization/Runner.java
@@ -1,7 +1,19 @@
 package com.practice.demoSerializationDeSerialization;
 
+import com.google.gson.Gson;
+
 public class Runner {
     public static void main(String[] args) {
         //achieve serialization using Gson or Jackson library
+
+        User user = new User("Niti", 33);
+        Gson gson = new Gson();
+        String convert = gson.toJson(user);
+        System.out.println(convert);
+
+        // deserialization with Gson
+        String deserialize = "{\"name\":\"Niti\",\"age\":33}";
+        User user2 = gson.fromJson(deserialize, User.class);
+        System.out.println(user2);
     }
 }
--- src/test/java/com.practice/demoSerializationDeSerialization/User.java
+++ src/test/java/com.practice/demoSerializationDeSerialization/User.java
@@ -5,6 +5,10 @@ public class User {
         private String name;
         private int age;
 
+        User(){
+
+        }
+
         User(String name, int age){
             this.name = name;
             this.age = age;
